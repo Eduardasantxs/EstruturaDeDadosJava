977fab613f264df3ddfd510babadc61d85191ee2
Eduardasantxs <89314350+Eduardasantxs@users.noreply.github.com>
2022-09-14 21:50:13 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
+
+
+        ListaEncadeada l = new ListaEncadeada();
+        System.out.println(l.ListaVazia());
+        l.AdicionarInicio(10);
+        System.out.println(l.ListaVazia());
+        l.AdicionarFinal(100);
+    }
+
+}
